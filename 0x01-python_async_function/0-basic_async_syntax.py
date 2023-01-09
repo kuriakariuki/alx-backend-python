d71981c16152b2b16e6f7956f4b19c1176065cf3
KuriaKariuki <kariukijohnk1@gmail.com>
2023-01-09 15:12:17 +0300

    Added 9 files

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-""" Takes int arg, waits for a random delay"""
+""" Takes int arg, waits for random delay """
 
 import asyncio
-import random 
+import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for random dalay between 0 and max_delay, returns that"""
+    """ Waits for random delay between 0 and max_delay, returns that """
     actual_delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(actual_delay)
     return actual_delay
-
-
-
